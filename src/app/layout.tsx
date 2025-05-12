365b45e1d4638d0e5e69e5cf80b10b781c1d9c11
Rodrigo Nunes <nunesrodrigo13@outlook.com>
2025-05-12 16:41:13 -0300

    feat: added partytown usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
- 
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata, Viewport } from "next";
 import Providers from "./providers";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google';
+import { Partytown } from "@qwik.dev/partytown/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,10 +42,32 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-K8ZLK3J6" />
+      <head>
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+        <script
+          type={"text/partytown"}
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K8ZLK3J6');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            title="Google Tag Manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8ZLK3J6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Providers>{children}</Providers>
       </body>
     </html>
